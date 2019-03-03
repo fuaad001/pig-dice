110eeb38536f9197568e4724e093a6034c6c1c8e
fuaad001 <husseinfuaad@hotmail.com>
2019-03-03 19:28:45 +0300

    Add user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,62 @@
 //Global variables
 var newPlayer1 = "";
 var newPlayer2 = "";
+
+//User Interface logic
+$(document).ready(function(){
+  $(".player-one").submit(function(event){
+    event.preventDefault();
+    var name = $(".player1-name").val();
+    newPlayer1 = new Player(name);
+    $(".login-player-one").slideUp();
+    $(".player1-board").fadeIn();
+    $("#name1").text(newPlayer1.name);
+    $("#turn-count1").text(0);
+    $("#total-score1").text(0);
+    $("#total-points1").text(0);
+  });
+  $(".player-two").submit(function(event){
+    event.preventDefault();
+    var name = $(".player2-name").val();
+    newPlayer2 = new Player(name);
+    $(".pig-image").slideUp();
+    $(".login-player-two").slideUp();
+    $(".player2-board").fadeIn();
+    $("#name2").text(newPlayer2.name);
+    $("#turn-count2").text(0);
+    $("#total-score2").text(0);
+    $("#total-points2").text(0);
+  });
+  $("#roll-dice1").click(function(){
+    play1();
+    $("#total-score1").text(newPlayer1.score);
+  });
+  $("#pass1").click(function(){
+    newPlayer1.tally();
+    $("#total-points1").text(newPlayer1.finalScore);
+    pass1();
+  });
+  $("#roll-dice2").click(function(){
+    play2();
+    $("#total-score2").text(newPlayer2.score);
+  });
+  $("#pass2").click(function(){
+    newPlayer2.tally();
+    $("#total-points2").text(newPlayer2.finalScore);
+    pass2();
+  });
+});
+
+var pass1 = function(){
+  $(document).ready(function(){
+    $(".player1-board").addClass("inactive");
+    $(".player1-board").removeClass("active");
+    $(".player2-board").addClass("active");
+    $(".player2-board").removeClass("inactive");
+    $(".submit1").attr("disabled", true);
+    $(".submit2").attr("disabled", false);
+    $(".pass1").attr("disabled", true);
+    $(".pass2").attr("disabled", false);
+    $(".roll-one2").hide();
+  });
+};
